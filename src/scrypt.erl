32dd2770b9dafac782a6be27513194dd291c76f4
Sergey Yelin <elinsn@gmail.com>
2021-04-09 09:19:52 +0300

    Add formatter

--- src/scrypt.erl
+++ src/scrypt.erl
@@ -23,7 +23,14 @@ stop(_State) ->
 %%% API
 %%%-------------------------------------------------------------------
 
--spec scrypt(binary(), binary(), non_neg_integer(), non_neg_integer(), non_neg_integer(), non_neg_integer()) -> binary().
+-spec scrypt(
+    binary(),
+    binary(),
+    non_neg_integer(),
+    non_neg_integer(),
+    non_neg_integer(),
+    non_neg_integer()
+) -> binary().
 scrypt(_Passwd, _Salt, _N, _R, _P, _Buflen) ->
     erlang:nif_error({not_loaded, [{module, ?MODULE}, {line, ?LINE}]}).
 
@@ -32,15 +39,16 @@ scrypt(_Passwd, _Salt, _N, _R, _P, _Buflen) ->
 %%%-------------------------------------------------------------------
 
 init() ->
-    SoName = case code:priv_dir(?APPNAME) of
-                 {error, bad_name} ->
-                     case filelib:is_dir(filename:join(["..", priv])) of
-                         true ->
-                             filename:join(["..", priv, ?LIBNAME]);
-                         _ ->
-                             filename:join([priv, ?LIBNAME])
-                     end;
-                 Dir ->
-                     filename:join(Dir, ?LIBNAME)
-             end,
+    SoName =
+        case code:priv_dir(?APPNAME) of
+            {error, bad_name} ->
+                case filelib:is_dir(filename:join(["..", priv])) of
+                    true ->
+                        filename:join(["..", priv, ?LIBNAME]);
+                    _ ->
+                        filename:join([priv, ?LIBNAME])
+                end;
+            Dir ->
+                filename:join(Dir, ?LIBNAME)
+        end,
     erlang:load_nif(SoName, 0).
